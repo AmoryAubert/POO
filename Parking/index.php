d12939992f97d6286c03c86766bb67382d654603
Amory Aubert <amory.aubert@gmail.com>
2019-04-30 16:55:20 +0200

    fin journee

--- Parking/index.php
+++ Parking/index.php
@@ -1,19 +1,48 @@
 <?php
 require 'parking.php';
+$bagnole1 = new voiture("BE 1-EXP-005","22/01/2017",mt_rand(5000,300000),"aston-martin","v12 vanquish 1997","gris foncé","1739");
+$bagnole2 = new voiture("BE 1-OQP-123","22/01/2017",mt_rand(5000,300000),"audi","r8 2016","rouge signalisation","1550");
+$bagnole3 = new voiture("DE ARN AU 333","22/01/2017",mt_rand(5000,300000),"audi","tt 2006","gris clair","1240");
+$bagnole4 = new voiture("BE 1-PLQ-069","22/01/2017",mt_rand(5000,300000),"dodge","viper gts 1997","rouge feu","1520");
+$bagnole5 = new voiture("IT 74FFJCSZ","22/01/2017",mt_rand(5000,300000),"ferrari","f430 spider 2005","gris acier","1520");
+$bagnole6 = new voiture("BE 1-XXX-619","22/01/2017",mt_rand(5000,300000),"ford","mustang 2015","rouge signalisation","1677");
+$bagnole7 = new voiture("FR 54DFEDFR","22/01/2017",mt_rand(5000,300000),"kia","optima 2015","blanche","1468");
+$bagnole8 = new voiture("DE KA PA 666","22/01/2017",mt_rand(5000,300000),"lotus","elise 2010","bleu outremer","860");
+$bagnole9 = new voiture("BE 1-KFC-444","22/01/2017",mt_rand(5000,300000),"mazda","mx-5 2015","rouge rubis","975");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Parking POO</title>
 </head>
 <body>
+    <main>
 <?php
-$bagnole1 = new voiture("EXP-005","22-01-2017","15234","Audi","R8","rouge","200");
 echo $bagnole1->display();
+echo $bagnole2->display();
+echo $bagnole3->display();
+echo $bagnole4->display();
+echo $bagnole5->display();
+echo $bagnole6->display();
+echo $bagnole7->display();
+echo $bagnole8->display();
+echo $bagnole9->display();
+
+// $bagnole1->roule();
+// echo $bagnole1->display();
+
+// $bagnole1->roule();
+// echo $bagnole1->display();
+
+// $bagnole1->roule();
+// echo $bagnole1->display();
 ?>
+    </main>
 </body>
 </html>
--- Parking/parking.php
+++ Parking/parking.php
@@ -1,6 +1,11 @@
 <?php
 
 class voiture{
+    public $libre;
+    public $origine;
+    public $etat;
+    public $age;
+
     public function __construct($immatriculation,$dateCirculation,$kilometre,$marque,$modele,$couleur,$poids){
         $this->immatriculation = $immatriculation;
         $this->dateCirculation = $dateCirculation;
@@ -9,8 +14,46 @@ class voiture{
         $this->modele = $modele;
         $this->couleur = $couleur;
         $this->poids = $poids;
+        $this->marque =="audi" ? $this->libre = "réservée" : $this->libre = "libre";
+        switch(substr($this->immatriculation,0,2)){
+            case "BE": $this->origine = "Belgique"; break;
+            case "DE": $this->origine = "Allemagne"; break;
+            case "FR": $this->origine = "France"; break;
+            case "IT": $this->origine = "Italie"; break;
+            default : "Inconnue";
+        }
     }
+
     public function display(){
-        return $this->modele;
+        if ($this->kilometre < 100000){
+            $this->etat = "peu servi";
+        } else if ($this->kilometre < 200000 && $this->kilometre > 100000){
+            $this->etat = "moyennement servi";
+        } else {
+            $this->etat = "beaucoup servi";
+        }
+        return "<div class='car'>
+                    <div class='car-desc'>
+                        <p>".ucfirst($this->marque)." ".ucfirst($this->modele)."</p>
+                        <p>Date de mise en circulation: ".$this->dateCirculation."</p>
+                        <p>Immatriculation: ".$this->immatriculation."</p>
+                        <p>Kilomètres: ".$this->kilometre." Km</p>
+                        <p>Origine: ".$this->origine."</p>
+                        <p>Couleur: ".$this->couleur."</p>
+                        <p>Poids: ".$this->poids." Kg</p>
+                        <p>Status: ".$this->libre."</p>
+                        <p>Etat: ".$this->etat."</p>
+                    </div>
+                    <img 
+                        src='assets/img/".$this->marque."-".str_replace(" ","-",$this->modele).".jpg' 
+                        alt='".$this->marque." ".$this->modele."'
+                        class='car-img'
+                    >
+                </div>"; // width='440'
+    }
+
+    public function roule(){
+        $this->kilometre += 100000;
+        $this->etat = $this->etat;
     }
 }
